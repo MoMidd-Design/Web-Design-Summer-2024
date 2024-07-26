3e23b79a70fa87b25bc89269f7ec12b9c358dd8c
MoMidd-Design <monroemiddleton0110@gmail.com>
2024-07-25 23:44:06 -0400

    Add files via upload

---
+++ Final Project/README.md
@@ -0,0 +1,2 @@
+# Final-Project-Web-Design-Project 2024
+ This includes my final project aswell as my overall homepage, which includes my final project
---
+++ Jquery/scripts.js
@@ -0,0 +1,13 @@
+$("button").click(
+    function(){
+      
+        $(".box").toggleClass("dark");
+        $(".box").toggleClass("dark");
+
+    }
+);
+
+$(".glow-btn").click(
+    funtion()
+    $(".box").addclass("glow");
+);
